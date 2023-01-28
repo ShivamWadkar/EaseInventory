062416d99c4b9febff9bfcd4fa91b1c32384ec82
ShivamWadkar <79800623+ShivamWadkar@users.noreply.github.com>
2023-01-28 15:52:12 +0530

    Authentication provided by default DAOAuthenticationProvider

--- src/main/java/com/inventory/easeinventory/config/SecurityConfig.java
+++ src/main/java/com/inventory/easeinventory/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.inventory.easeinventory.config;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,13 +16,19 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/auth/login").permitAll()
-                .requestMatchers("/add").authenticated()
+        http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/auth/login","/admin/signup").permitAll()
+                .requestMatchers("/add","/buyer/getAll").authenticated()
                 .and().formLogin()
                 .and().logout()
-                .and().httpBasic()
-                .and().csrf().disable();
+                .and().httpBasic();
         return http.build();
     }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
+
--- src/main/java/com/inventory/easeinventory/services/AdminServiceImpl.java
+++ src/main/java/com/inventory/easeinventory/services/AdminServiceImpl.java
@@ -1,6 +1,7 @@
 package com.inventory.easeinventory.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.inventory.easeinventory.entity.Admin;
@@ -11,10 +12,13 @@ public class AdminServiceImpl implements AdminService{
 
 	@Autowired
 	private AdminRepository adminRepository;
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	@Override
 	public Admin saveAdmin(Admin admin) {
-		
+
+		String encodedPassword = (passwordEncoder.encode(admin.getPassword()));
+		admin.setPassword(encodedPassword);
 		return adminRepository.save(admin);
 	}
 
